3a999e8fef190ff97bde2e3b923b07b4727c2814
Badulundabad <greenday407@mail.ru>
2021-06-11 17:15:40 +0300

    hz

--- Model/NET/Updating.cs
+++ Model/NET/Updating.cs
@@ -3,6 +3,8 @@ using RealtyModel.Model;
 using RealtyModel.Model.Derived;
 using RealtyModel.Model.Operations;
 using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace RealtorServer.Model.NET
 {
@@ -28,30 +30,49 @@ namespace RealtorServer.Model.NET
             Response response = new Response(Array.Empty<Byte>(), ErrorCode.Unknown);
             try
             {
-                Flat modifiedflat = BinarySerializer.Deserialize<Flat>(operation.Data);
+                Flat modifiedFlat = BinarySerializer.Deserialize<Flat>(operation.Data);
                 using (RealtyContext context = new RealtyContext())
                 {
-                    if (modifiedflat.Album.PhotoCollection.Length > 0)
+                    Flat flatToModify = context.Flats.Local.First(flat => flat.Id == modifiedFlat.Id);
+                    if (modifiedFlat.Album.PhotoCollection.Length > 0)
                     {
-                        Album oldAlbum = context.Albums.Find(modifiedflat.AlbumId);
+                        Album oldAlbum = context.Albums.Find(modifiedFlat.AlbumId);
                         if (oldAlbum != null)
-                            oldAlbum.PhotoCollection = modifiedflat.Album.PhotoCollection;
+                            oldAlbum.PhotoCollection = modifiedFlat.Album.PhotoCollection;
                     }
-                    if (modifiedflat.Album.Location.Length > 0)
+                    if (modifiedFlat.Album.Location.Length > 0)
                     {
-                        Album oldAlbum = context.Albums.Find(modifiedflat.AlbumId);
+                        Album oldAlbum = context.Albums.Find(modifiedFlat.AlbumId);
                         if (oldAlbum != null)
-                            oldAlbum.Location = modifiedflat.Album.Location;
+                            oldAlbum.Location = modifiedFlat.Album.Location;
                     }
-                    if (modifiedflat.Location.Street.Id > 0)
-                        modifiedflat.Location.Street = context.Streets.Find(modifiedflat.Location.Street.Id) ?? modifiedflat.Location.Street;
+                    //if (modifiedFlat.Location.Street.Id > 0)
+                    //    flatToModify.Location.Street = context.Streets.Find(modifiedFlat.Location.Street.Id) ?? modifiedFlat.Location.Street;
+                    if (!modifiedFlat.Location.Equals(flatToModify.Location))
+                    {
+                        Debug.WriteLine("locations are not equal");
+                        flatToModify.Location.GetValues(modifiedFlat.Location);
+                        if (flatToModify.Location.Street.Id != modifiedFlat.Location.Street.Id)
+                        {
+                            Debug.WriteLine("streets are not equal");
+                            flatToModify.Location.Street = context.Streets.Local.First(street => street.Id == flatToModify.Location.StreetId);
+                            flatToModify.Location.StreetId = flatToModify.Location.Street.Id;
+                        }
+                    }
+                    flatToModify.Agent = modifiedFlat.Agent;
+                    flatToModify.Cost = modifiedFlat.Cost;
+                    flatToModify.CustomerName = modifiedFlat.CustomerName;
+                    flatToModify.CustomerPhoneNumbers = modifiedFlat.CustomerPhoneNumbers;
+                    flatToModify.GeneralInfo = modifiedFlat.GeneralInfo;
+                    flatToModify.Info = modifiedFlat.Info;
+                    flatToModify.HasExclusive = modifiedFlat.HasExclusive;
+                    flatToModify.LastCallTime = modifiedFlat.LastCallTime;
+                    flatToModify.Preview = modifiedFlat.Preview;
+                    flatToModify.LastUpdateTime = DateTime.Now;
 
-                    modifiedflat.LastUpdateTime = DateTime.Now;
-
-                    context.Flats.Add(modifiedflat);
                     context.SaveChanges();
-                    LogInfo($"Flat has registered");
-                    response.Code = ErrorCode.FlatAddedSuccessfuly;
+                    LogInfo($"Flat has modified");
+                    response.Code = ErrorCode.FlatUpdatedSuccessfuly;
                 }
             }
             catch (Exception ex)
